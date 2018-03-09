7677a4ea3c476e135d9d50e33dc8bd87f179d506
andreas <andreas@localhost.localdomain>
2018-03-09 16:19:16 +0100

    run pfasst with degree=2

---
+++ test_pfasst/cmp_degree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# vim: ai:ts=2:sw=2:et
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append('../')
+
+#from diff import getDiff
+
+def getDiff(ref, res):
+  refD=np.loadtxt(ref)
+  resD=np.loadtxt(res)
+  resD -= refD
+  return [np.linalg.norm(resD),np.amax(resD)]
+
+nSteps=[2,4,8,16,32,64]
+nIters=[1,2,4,8]
+nCores=[2,4]
+
+te=20.0
+degree=2
+resFormat="heat2dMoving_result_pfasst_nCoarse_1_nFine_1_nIter_%(nIter)d_nSteps_%(nSteps)d_nCore_%(nCore)d.csv"
+refFile="../../../../movingHeatSDC/movingHeat/movingHeat/heat2d_for_pfasst/degree_%(deg)d/heat_ros2_8192.csv" % {'deg':degree}
+for nCore in nCores:
+  diffData=np.zeros((len(nSteps), len(nIters)))
+  for nIterP in range(len(nIters)):
+    nIter = nIters[nIterP]
+    for nStepP in range(len(nSteps)):
+      nStep=nSteps[nStepP]
+      data={'nIter' : nIter, 'nSteps' : nStep, 'nCore':nCore}
+      res=resFormat % data
+      diff=[0,0]
+      if os.path.isfile(res):
+        diff=getDiff(refFile, res)
+      diffData[nStepP,nIterP]=diff[1]
+  print diffData
+  lines=plt.loglog(te/np.array(nSteps), diffData)
+  plt.title('nCore=%(nCore)d' % data)
+  plt.legend(lines,['nIter=%d' %k for k in nIters])
+  plt.show()
+
--- test_pfasst/run.sh
+++ test_pfasst/run.sh
@@ -6,13 +6,14 @@ nCoarses="1 2 4"
 nIters="1 2 4 8"
 nCores="2 4"
 
-binFix="${bindir}/FE_heat2d_pfasst color=false abs_res_tol=10e-17 --num_elements=10 --tend=20.0"
+binFix="${bindir}/FE_heat2d_pfasst color=false abs_res_tol=10e-17 --num_elements=10 tend=20.0"
 for nStep in $nSteps; do
   for nCore in $nCores; do
     for nIter in $nIters; do
       clog="log_${nIter}_${nStep}_${nCore}"
       echo "$clog"
-      mpirun -np $nCore ${binFix} --num_iters=$nIter --num_steps=$nStep --nFine=$nFine --nCoarse=$nCoarse > $clog
+      nStep2=$(($nStep*$nCore))
+      mpirun -np $nCore ${binFix} --num_iters=$nIter --num_steps=$nStep2 > $clog
     done
   done
 done
