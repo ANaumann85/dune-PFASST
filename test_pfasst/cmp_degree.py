54ffc3f64726db9c503afbfa4969345306391587
andreas <andreas@localhost.localdomain>
2018-03-10 11:31:51 +0100

    auch mehrere sweep schritte bringen nicht den gewinn

--- test_pfasst/cmp_degree.py
+++ test_pfasst/cmp_degree.py
@@ -14,30 +14,36 @@ def getDiff(ref, res):
   refD=np.loadtxt(ref)
   resD=np.loadtxt(res)
   resD -= refD
-  return [np.linalg.norm(resD),np.amax(resD)]
+  return [np.linalg.norm(resD),np.amax(np.abs(resD))]
 
-nSteps=[2,4,8,16,32,64]
-nIters=[1,2,4,8]
+if len(sys.argv) != 2:
+  print("usage %s <degree>\n" % argv[0])
+  sys.exit(1)
+nSteps=[2,4,8,16,32,64, 128, 256]
+nIters=[1,2,3,4,8]
 nCores=[2,4]
 
 te=20.0
-degree=2
-resFormat="heat2dMoving_result_pfasst_nCoarse_1_nFine_1_nIter_%(nIter)d_nSteps_%(nSteps)d_nCore_%(nCore)d.csv"
+degree=int(sys.argv[1])
+resFormat="heat2dMoving_result_pfasst_nCoarse_%(nCoarse)d_nFine_%(nFine)d_nIter_%(nIter)d_nSteps_%(nSteps)d_nCore_%(nCore)d.csv"
+#refFile="../../../../movingHeatSDC/movingHeat/movingHeat/heat2d_for_pfasst/degree_%(deg)d/heat_ros2_81920.csv" % {'deg':degree}
 refFile="../../../../movingHeatSDC/movingHeat/movingHeat/heat2d_for_pfasst/degree_%(deg)d/heat_ros2_8192.csv" % {'deg':degree}
+
 for nCore in nCores:
-  diffData=np.zeros((len(nSteps), len(nIters)))
+  diffData=np.zeros((len(nSteps), len(nIters)+1))
   for nIterP in range(len(nIters)):
     nIter = nIters[nIterP]
     for nStepP in range(len(nSteps)):
       nStep=nSteps[nStepP]
-      data={'nIter' : nIter, 'nSteps' : nStep*nCore, 'nCore':nCore}
+      data={'nIter' : nIter, 'nSteps' : nStep*nCore, 'nCore':nCore, 'nCoarse':1 , 'nFine':1}
       res=resFormat % data
       diff=[0,0]
       if os.path.isfile(res):
         diff=getDiff(refFile, res)
-      diffData[nStepP,nIterP]=diff[1]
-  print diffData
-  lines=plt.loglog(te/np.array(nSteps), diffData)
+      diffData[nStepP,0]=nStep*nCore
+      diffData[nStepP,nIterP+1]=diff[1]
+  print np.array2string(diffData,precision=3)
+  lines=plt.loglog(te/np.array(nSteps), diffData[:,1:])
   plt.title('nCore=%(nCore)d' % data)
   plt.legend(lines,['nIter=%d' %k for k in nIters])
   plt.show()
--- test_pfasst/run.sh
+++ test_pfasst/run.sh
@@ -1,12 +1,14 @@
 bindir="../../release/src/examples/FE_heat2d/"
 
-nSteps="2 4 8 16 32 64"
+nSteps="2 4 8 16 32 64 128 256"
 nFines="1 2 4"
 nCoarses="1 2 4"
-nIters="1 2 4 8"
+nIters="1 2 3 4 8"
+nIters="3"
 nCores="2 4"
+#nCores="4"
 
-binFix="${bindir}/FE_heat2d_pfasst color=false abs_res_tol=10e-17 --num_elements=10 tend=20.0"
+binFix="${bindir}/FE_heat2d_pfasst color=false abs_res_tol=10e-17 --num_elements=10 tend=20.0 --nCoarse=1 --nFine=1"
 for nStep in $nSteps; do
   for nCore in $nCores; do
     for nIter in $nIters; do
